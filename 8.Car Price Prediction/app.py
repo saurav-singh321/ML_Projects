4a52987775c20b9dca5ee8a8fde7fa588880a1ad
saurav-singh321 <56493291+saurav-singh321@users.noreply.github.com>
2022-09-25 11:37:38 +0530

    Add files via upload

---
+++ 8.Car Price Prediction/app.py
@@ -0,0 +1,24 @@
+import pickle
+from flask import Flask,request,app,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+#Load the model
+model = pickle.load(open('model1.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()] # whatever we get data, it is stored in data
+    new_data = np.array(data).reshape(1,-1)
+    print(new_data)
+    output = model.predict(new_data)[0]
+    return render_template('index.html',prediction_text = f"the price of car is {output} lakhs")
+
+if __name__ == '__main__':
+    app.run(debug=True)
